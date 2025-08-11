0f5b2c13ee0690e739418f73de4bed79acae7cc3
Magnus Larsen <magnushlarsen01@protonmail.com>
2025-08-11 15:50:16 +0200

    add steam to floating group

--- config.h
+++ config.h
@@ -39,6 +39,8 @@ static const Rule rules[] = {
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "steam",    NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "steamwebhelper",    NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "Alacritty", NULL,   NULL,           0,         0,          1,           0,        -1 },
